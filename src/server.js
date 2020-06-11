7d1cf2d5b48c0a3024563f40fd11cd65f3b433a0
Silva404 <brenosilva0252@gmail.com>
2020-06-11 12:37:01 -0300

    ok minha segunda pag nao funciona

--- src/server.js
+++ src/server.js
@@ -4,15 +4,24 @@ const server = express()
 //configurar pasta pública
 server.use(express.static('public'))
 
+// chamando a template engine
+const nunjucks = require('nunjucks')
+// configurando o nunjucks
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+// express será meu servidor e noCache que é pra ele sempre ficar atualizado, sem resquicios de uma versão antiga dos arquivos.
+
 // page home
 // req = requisition
 // res = response
 server.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    return res.render('index.html')
 })
 
-server.get('/create-point', (req, res) => {
-    res.sendFile(__dirname + '/views/create-point.html')
+server.get('create-point', (req, res) => {
+    return res.render('create-point.html')
 })
 
 // ligando o servidor
