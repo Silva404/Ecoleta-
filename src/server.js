b712cb584453e0b953ccc2c162bb873970d66b75
Silva404 <brenosilva0252@gmail.com>
2020-06-11 12:24:12 -0300

    iniciando o servidor

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const server = express()
 
+//configurar pasta pública
+server.use(express.static('public'))
+
 // page home
 // req = requisition
 // res = response
 server.get('/', (req, res) => {
-    res.send('eae mah')
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+server.get('/create-point', (req, res) => {
+    res.sendFile(__dirname + '/views/create-point.html')
 })
 
 // ligando o servidor
